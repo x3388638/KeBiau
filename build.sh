1e257328e58d0b764554815ae8f587e080ac979a
YY <z3388638@gmail.com>
2017-09-03 17:17:21 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+git checkout gh-pages
+git merge master -X theirs
+npm install
+npm run build
+mkdir temp
+mv -v ./* ./__temp__
+rm -rf __temp__/
+git add .
+git commit -m 'build'
+git push
+git checkout master
