b2effd255268be463d762e335aad781a9e398c59
YY <z3388638@gmail.com>
2017-09-03 20:44:50 +0800

    add fb sdk

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,25 @@ import { Container } from 'reactstrap';
 
 import Navigation from './Navigation.jsx';
 
+import CONFIG from './config.js';
+
 import './App.css';
 
 export default class App extends React.Component {
+	componentDidMount() {
+		window.fbAsyncInit = function() {
+			window.FB.init({
+				appId      : CONFIG.FB.AppID,
+				xfbml      : true,
+				version    : 'v2.10'
+			});
+			window.FB.AppEvents.logPageView();
+			window.FB.getLoginStatus(function(response) {
+				console.log(response);
+			});
+		};
+	}
+
 	render() {
 		return (
 			<div>
