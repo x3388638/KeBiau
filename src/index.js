bb328cfa60eb1e95fec66f4ebe6686c549c13fe9
YY <z3388638@gmail.com>
2017-09-04 23:35:33 +0800

    add firebase config

--- src/index.js
+++ src/index.js
@@ -14,6 +14,17 @@ import Exchange from './Exchange.jsx';
 
 import registerServiceWorker from './registerServiceWorker';
 
+// Initialize Firebase
+const config = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+};
+window.firebase.initializeApp(config);
+
 ReactDOM.render(
 	<Router>
 		<Switch>
