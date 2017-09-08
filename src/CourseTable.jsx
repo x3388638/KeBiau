02593f1240e5fb45239f42e7c91d545e277e5ca8
YY <z3388638@gmail.com>
2017-09-08 18:03:20 +0800

    render courseGrid

--- src/CourseTable.jsx
+++ src/CourseTable.jsx
@@ -3,6 +3,27 @@ import {
 	Table
 } from 'reactstrap';
 
+class CourseGrid extends React.Component {
+	render() {
+		let gridStyle = {
+			verticalAlign: 'middle'
+		};
+
+		this.props.bg && (gridStyle.background = this.props.bg);
+		return (
+			<td className="courseGrid" style={gridStyle} rowSpan={this.props.rowspan || 1}>
+				{ this.props.title && 
+					<div className="title">{this.props.title}</div>
+				}
+
+				{ this.props.desc && 
+					<div className="desc">{this.props.desc}</div>
+				}
+			</td>
+		)
+	}
+}
+
 export default class CourseTable extends React.Component {
 	render() {
 		const timeNo = ['a/08', 'b/09', 'c/10', 'd/11', 'z/12', 'e/13', 'f/14', 'g/15', 'h/16', 'i/17', 'j/18', 'k/19', 'l/20', 'm/21'];
@@ -12,15 +33,16 @@ export default class CourseTable extends React.Component {
 					<thead>
 						<tr>
 							<th style={{width: '60px'}}></th>
-							<th className="text-center" style={{width: 'auto'}}>星期一</th>
-							<th className="text-center" style={{width: 'auto'}}>星期二</th>
-							<th className="text-center" style={{width: 'auto'}}>星期三</th>
-							<th className="text-center" style={{width: 'auto'}}>星期四</th>
-							<th className="text-center" style={{width: 'auto'}}>星期五</th>
+							<th className="text-center">星期一</th>
+							<th className="text-center">星期二</th>
+							<th className="text-center">星期三</th>
+							<th className="text-center">星期四</th>
+							<th className="text-center">星期五</th>
 							{
 								this.props.tableData.sat &&
 								<th className="text-center" style={{width: 'auto'}}>星期六</th>
 							}
+							
 							{
 								this.props.tableData.sun &&
 								<th className="text-center" style={{width: 'auto'}}>星期日</th>
@@ -31,14 +53,12 @@ export default class CourseTable extends React.Component {
 						{
 							timeNo.map((t, i) => {
 								return (
-									<tr key={i}>
+									<tr data-time={t} key={i}>
 										<td className="text-center">{t}</td>
 										{
-												Object.values(this.props.tableData.course[t]).map((val, i) => {
-													return (
-														<td key={i}>{JSON.stringify(val)}</td>
-													)
-												})
+											Object.values(this.props.tableData.course[t]).map((courseData, i) => {
+												return <CourseGrid key={i} {...courseData}/>
+											})
 										}
 									</tr>
 								)
