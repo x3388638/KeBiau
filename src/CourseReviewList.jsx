d4a4e7426ed044aa875e813b9d7ed781b42ff8f6
YY <z3388638@gmail.com>
2017-10-10 15:06:05 +0800

    ReviewItem layout

--- src/CourseReviewList.jsx
+++ src/CourseReviewList.jsx
@@ -1,51 +1,68 @@
 import React from 'react';
 import {
+	Container,
 	Row,
-	Col
+	Col,
+	Card,
+	CardColumns,
+	CardBlock
 } from 'reactstrap';
+
+import './CourseReviewList.css';
+
+class ReviewItem extends React.Component {
+	render() {
+		return (
+			<Card className="mb-2 ReviewItem">
+				<CardBlock className="pb-2">
+					<div className="mb-2">
+						<table>
+							<tbody>
+								<tr>
+									<td className="pr-2" rowSpan="2">
+										<a href="#">
+											<img className="ReviewItem__userImg" src="https://graph.facebook.com/1513796702039003/picture" alt=""/>
+										</a>
+									</td>
+									<td>
+										<a href="#">
+											<span className="ReviewItem__username">Y.Y. ChaNg</span>
+										</a>
+									</td>
+								</tr>
+								<tr>
+									<td>
+										<span className="ReviewItem__date">2017/07/07</span>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					<div>
+						<div className="ReviewItem__title">
+							103501 Linux 系統管理實務 | 練Ｏ明
+						</div>
+						<div>
+							推推，老師很帥，聲音很有磁性，內容豐富精實有用又有趣ＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲ
+						</div>
+					</div>
+					<hr style={{marginBottom: '5px'}} />
+					<div style={{display: 'flex'}}>
+						<span className="text-center ReviewItem__btnLike"><i className="fa fa-thumbs-o-up" aria-hidden="true"></i> 116</span>
+						<span className="text-center ReviewItem__btnDislike"><i className="fa fa-thumbs-o-down" aria-hidden="true"></i> 23</span>
+					</div>
+				</CardBlock>
+			</Card>
+		);
+	}
+}
 
 export default class CourseReviewList extends React.Component {
 	render() {
 		return (
-			<Row>
-				<Col xs="12" sm="12" md="6">
-					<div style={{border: '1px solid rgba(0,0,0,.125)', borderRadius: '.20rem', padding: '10px'}}>
-						<div className="mb-2">
-							<table>
-								<tbody>
-									<tr>
-										<td className="pr-2" rowSpan="2">
-											<a href="#">
-												<img src="https://graph.facebook.com/1513796702039003/picture" alt="" style={{borderRadius: '30px'}}/>
-											</a>
-										</td>
-										<td>
-											<a href="#">
-												<span style={{color: '#365899', fontSize: '14px', fontWeight: 'bold'}}>Y.Y. ChaNg</span>
-											</a>
-										</td>
-									</tr>
-									<tr>
-										<td>
-											<span style={{fontSize: '12px', color:'#90949c'}}>2017/07/07</span>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-						<div>
-							<div style={{fontWeight: 'bold'}}>
-								103501 Linux 系統管理實務 | 練喆明
-							</div>
-							<div>
-								推推，老師很帥，聲音很有磁性，內容豐富精實有用又有趣ＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲＲ
-							</div>
-						</div>
-						<hr style={{marginBottom: '10px'}} />
-						<div>789</div>
-					</div>
-				</Col>
-			</Row>
+			<CardColumns>
+				<ReviewItem />
+			</CardColumns>
 		)
 	}
 }
