9148d9de42f2ea97ac50211d01f3d0c9478c16fe
YY <z3388638@gmail.com>
2018-07-03 19:00:54 +0800

    add loading icon & styled component

--- src/CourseList.jsx
+++ src/CourseList.jsx
@@ -3,6 +3,7 @@ import {
 	Table,
 	Button
 } from 'reactstrap';
+import Loading from './Loading';
 
 class DeptSelector extends React.Component {
 	constructor(props) {
@@ -71,48 +72,54 @@ export default class CourseList extends React.Component {
 			<div>
 				<DeptSelector { ...this.props } />
 				<div style={{overflow: 'auto', maxHeight: '487px', background: '#fff', width: '100%'}}>
-					<Table hover responsive striped>
-						<thead>
-							<tr>
-								<th>課號</th>
-								<th>課程名稱</th>
-								<th>班別</th>
-								<th>時段</th>
-								<th>授課地點</th>
-								<th>教師</th>
-								<th>年級</th>
-								<th></th>
-								<th></th>
-							</tr>
-						</thead>
-						<tbody>
-							{
-								Object.keys(list).map((val) => {
-									if (this.props.filterConflict && list[val].isConflict) {
-										return null;
-									}
 
-									return (
-										<tr key={val} data-uuid={val}>
-											<td>{list[val].cid}</td>
-											<td>{list[val].cname}</td>
-											<td>{list[val].classes}</td>
-											<td>{list[val].time}</td>
-											<td>{list[val].location}</td>
-											<td>{list[val].teacher}</td>
-											<td>{list[val].grade}</td>
-											<td>
-												<a href={`https://ccweb.ncnu.edu.tw/student/aspmaker_course_opened_detail_viewview.php?showdetail=&year=${list[val].year}&courseid=${list[val].cid}&class=${list[val].classes}&modal=2`} target="_blank">課綱 <i className="fa fa-external-link" aria-hidden="true"></i></a>
-											</td>
-											<td>
-												<Button color="success" size="sm" disabled={list[val].isConflict ? true : false} onClick={() => {this.props.onAddCourse(list[val], true)}}>{list[val].isConflict ? '衝堂' : '加入'}</Button>
-											</td>
-										</tr>
-									);
-								})
-							}
-						</tbody>
-					</Table>
+					{ !list.length &&
+						<Loading />
+					}
+					{ !!list.length &&
+						<Table hover responsive striped>
+							<thead>
+								<tr>
+									<th>課號</th>
+									<th>課程名稱</th>
+									<th>班別</th>
+									<th>時段</th>
+									<th>授課地點</th>
+									<th>教師</th>
+									<th>年級</th>
+									<th></th>
+									<th></th>
+								</tr>
+							</thead>
+							<tbody>
+								{
+									Object.keys(list).map((val) => {
+										if (this.props.filterConflict && list[val].isConflict) {
+											return null;
+										}
+
+										return (
+											<tr key={val} data-uuid={val}>
+												<td>{list[val].cid}</td>
+												<td>{list[val].cname}</td>
+												<td>{list[val].classes}</td>
+												<td>{list[val].time}</td>
+												<td>{list[val].location}</td>
+												<td>{list[val].teacher}</td>
+												<td>{list[val].grade}</td>
+												<td>
+													<a href={`https://ccweb.ncnu.edu.tw/student/aspmaker_course_opened_detail_viewview.php?showdetail=&year=${list[val].year}&courseid=${list[val].cid}&class=${list[val].classes}&modal=2`} target="_blank">課綱 <i className="fa fa-external-link" aria-hidden="true"></i></a>
+												</td>
+												<td>
+													<Button color="success" size="sm" disabled={list[val].isConflict ? true : false} onClick={() => {this.props.onAddCourse(list[val], true)}}>{list[val].isConflict ? '衝堂' : '加入'}</Button>
+												</td>
+											</tr>
+										);
+									})
+								}
+							</tbody>
+						</Table>
+					}
 				</div>
 			</div>
 		);
---
+++ src/Loading.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BarLoader } from 'react-spinners';
+import styled from 'styled-components';
+
+const LoadingWrapper = styled.div`
+	display: flex;
+    justify-content: center;
+    margin-top: 20px;
+`;
+
+export default class Loading extends React.PureComponent {
+	render() {
+		return (
+			<LoadingWrapper>
+				<BarLoader
+					color="#666"
+					loading
+					height={ 5 }
+					width={ 200 }
+				/>
+			</LoadingWrapper>
+		)
+	}
+}
