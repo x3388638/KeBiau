908553a34fafa9200bd9bc4ae028d9a2c1b2dfd7
YY <z3388638@gmail.com>
2018-07-16 17:19:05 +0800

    update octocat; use styled-components for Navigation

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -11,6 +11,22 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const NavigationBar = styled(Navbar)`
+	z-index: 5;
+	padding: '0.3rem 1rem';
+`;
+
+const UserImg = styled.img`
+	border-radius: 50px;
+`;
+
+const UserName = styled.span`
+	@media (max-width: 900px) and (min-width: 576px) {
+		display: none;
+	}
+`;
 
 export default class Navigation extends React.Component {
 	constructor(props) {
@@ -69,74 +85,72 @@ export default class Navigation extends React.Component {
 		const loginStatusInit = !!user;
 		const isLogin = loginStatusInit && !!user.uid;
 		return (
-			<div>
-				<Navbar color="dark" dark expand="sm" style={{zIndex: 5, padding: '0.3rem 1rem'}}>
-					<NavbarBrand href={process.env.REACT_APP_BASE_URL}>
-						<img src={`${process.env.REACT_APP_BASE_URL}/logo.png`} alt="" height="31px"/> 自己的課表自己排 2.0
-					</NavbarBrand>
-					<NavbarToggler right onClick={this.toggleNavbar} />
-					<Collapse isOpen={this.state.navbarIsOpen} navbar>
-						<Nav navbar>
+			<NavigationBar color="dark" dark expand="sm">
+				<NavbarBrand href={process.env.REACT_APP_BASE_URL}>
+					<img src={`${process.env.REACT_APP_BASE_URL}/logo.png`} alt="" height="31px"/> 自己的課表自己排 2.0
+				</NavbarBrand>
+				<NavbarToggler right onClick={this.toggleNavbar} />
+				<Collapse isOpen={this.state.navbarIsOpen} navbar>
+					<Nav navbar>
+						<NavItem>
+							<Link className={route.match.path === '/' ? 'active nav-link' : ' nav-link'} to="/">
+								<i className="fa fa-table" aria-hidden="true"></i> <span className="d-sm-none d-md-inline">我的課表</span>
+							</Link>
+						</NavItem>
+						<NavItem>
+							<Link className={route.match.path === '/exchange' ? 'active nav-link' : ' nav-link'} to="/exchange">
+								<i className="fa fa-exchange" aria-hidden="true"></i> <span className="d-sm-none d-md-inline">換課平台</span>
+							</Link>
+						</NavItem>
+						<NavItem>
+							<Link className={route.match.path === '/review' ? 'active nav-link' : ' nav-link'} to="/review">
+								<i className="fa fa-thumbs-up" aria-hidden="true"></i> <span className="d-sm-none d-md-inline">課程評價</span>
+							</Link>
+						</NavItem>
+						<NavItem>
+							<NavLink href="https://github.com/x3388638/KeBiau/issues" target="_blank">
+								<span>
+									<i className="fa fa-exclamation-circle" aria-hidden="true"></i> <span className="d-sm-none d-md-inline">回報</span>
+								</span>
+							</NavLink>
+						</NavItem>
+					</Nav>
+					<Nav className="ml-auto" navbar>
+						{
+							loginStatusInit && isLogin &&
 							<NavItem>
-								<Link className={route.match.path === '/' ? 'active nav-link' : ' nav-link'} to="/">
-									<i className="fa fa-table" aria-hidden="true"></i> <span className="d-sm-none d-md-inline">我的課表</span>
-								</Link>
-							</NavItem>
-							<NavItem>
-								<Link className={route.match.path === '/exchange' ? 'active nav-link' : ' nav-link'} to="/exchange">
-									<i className="fa fa-exchange" aria-hidden="true"></i> <span className="d-sm-none d-md-inline">換課平台</span>
-								</Link>
-							</NavItem>
-							<NavItem>
-								<Link className={route.match.path === '/review' ? 'active nav-link' : ' nav-link'} to="/review">
-									<i className="fa fa-thumbs-up" aria-hidden="true"></i> <span className="d-sm-none d-md-inline">課程評價</span>
-								</Link>
-							</NavItem>
-							<NavItem>
-								<NavLink href="https://github.com/x3388638/KeBiau/issues" target="_blank">
-									<span>
-										<i className="fa fa-exclamation-circle" aria-hidden="true"></i> <span className="d-sm-none d-md-inline">回報</span>
-									</span>
+								<NavLink href={`https://fb.com/${user.uid}`} target="_blank">
+									<UserImg src={`https://graph.facebook.com/${user.uid}/picture`} alt="" height="21" />
+									<UserName className="ml-2">{ user.displayName }</UserName>
 								</NavLink>
 							</NavItem>
-						</Nav>
-						<Nav className="ml-auto" navbar>
-							{
-								loginStatusInit && isLogin &&
-								<NavItem>
-									<NavLink href={`https://fb.com/${user.uid}`} target="_blank">
-										<img src={`https://graph.facebook.com/${user.uid}/picture`} alt="" height="21" style={{borderRadius: '50px'}} />
-										<span className="ml-2 d-sm-none d-md-inline">{user.displayName}</span>
+						}
+						{
+							loginStatusInit &&
+							<NavItem>
+								{
+									isLogin &&
+									<NavLink id="LogoutBtn" href="#" onClick={this.handleLogout}>
+										<i className="fa fa-sign-out" aria-hidden="true"></i> <span className="d-inline d-sm-none">登出</span>
+										<Tooltip delay={{ show: 0, hide: 0 }} placement="left" isOpen={this.state.logoutTooltip} target="LogoutBtn" toggle={this.toggleLogoutTooltip}>
+											登出
+										</Tooltip>
 									</NavLink>
-								</NavItem>
-							}
-							{
-								loginStatusInit &&
-								<NavItem>
-									{
-										isLogin &&
-										<NavLink id="LogoutBtn" href="#" onClick={this.handleLogout}>
-											<i className="fa fa-sign-out" aria-hidden="true"></i> <span className="d-inline d-sm-none">登出</span>
-											<Tooltip delay={{ show: 0, hide: 0 }} placement="left" isOpen={this.state.logoutTooltip} target="LogoutBtn" toggle={this.toggleLogoutTooltip}>
-												登出
-											</Tooltip>
-										</NavLink>
-									}
-									{
-										isLogin ||
-										<NavLink id="LoginBtn" href="#" onClick={this.handleLogin}>
-											<i className="fa fa-facebook-square" aria-hidden="true"></i>
-											<Tooltip delay={{ show: 0, hide: 0 }} placement="left" isOpen={this.state.loginTooltip} target="LoginBtn" toggle={this.toggleLoginTooltip}>
-												登入 Facebook
-											</Tooltip>
-										</NavLink>
-									}
-								</NavItem>
-							}
-						</Nav>
-					</Collapse>
-				</Navbar>
-			</div>
+								}
+								{
+									isLogin ||
+									<NavLink id="LoginBtn" href="#" onClick={this.handleLogin}>
+										<i className="fa fa-facebook-square" aria-hidden="true"></i>
+										<Tooltip delay={{ show: 0, hide: 0 }} placement="left" isOpen={this.state.loginTooltip} target="LoginBtn" toggle={this.toggleLoginTooltip}>
+											登入 Facebook
+										</Tooltip>
+									</NavLink>
+								}
+							</NavItem>
+						}
+					</Nav>
+				</Collapse>
+			</NavigationBar>
 		);
 	}
 }
