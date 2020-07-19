3a232d5c182fb31cfa94bd993381210803bc6db7
YY <z3388638@gmail.com>
2020-07-19 16:06:19 +0800

    Updates

---
+++ precache-manifest.e1bfa2a06e3314d1b0e21e27dd28b9da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6873cf9f001e55779e34e9d6e5f8ab45",
+    "url": "/KeBiau/index.html"
+  },
+  {
+    "revision": "5a026a5b126ac2cb8a78",
+    "url": "/KeBiau/static/css/2.4a511337.chunk.css"
+  },
+  {
+    "revision": "572106e11ab4a6d6ef2f",
+    "url": "/KeBiau/static/css/main.d3b38a47.chunk.css"
+  },
+  {
+    "revision": "5a026a5b126ac2cb8a78",
+    "url": "/KeBiau/static/js/2.6b8de05b.chunk.js"
+  },
+  {
+    "revision": "40c3a33808930d3a21d6f2780ce2fb6e",
+    "url": "/KeBiau/static/js/2.6b8de05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "572106e11ab4a6d6ef2f",
+    "url": "/KeBiau/static/js/main.19dc1241.chunk.js"
+  },
+  {
+    "revision": "5cd4f3a733689bfa231e",
+    "url": "/KeBiau/static/js/runtime-main.4fb8a97d.js"
+  }
+]);
